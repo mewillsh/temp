258c9a2de5211baebe3d6f0595e5374aa8ad5aba
mewillsh <132931280+mewillsh@users.noreply.github.com>
2024-01-09 13:49:32 +0000

    Pending changes exported from your codespace

---
+++ collage_bootcamp/day_6/one.js
@@ -0,0 +1,28 @@
+function msg()
+{
+  alert("hello javatpoint");  
+}
+function add(){
+  var x=40;
+  var y=8;
+  var sum=x+y;
+  document.write(sum)
+}
+function sub(){
+  var x=40;
+  var y=8;
+  var su=x-y;
+  document.write(su)
+}
+function produc(){
+  var x=40;
+  var y=8;
+  var produc=x*y;
+  document.write(produc)
+}
+function divide(){
+  var x=40;
+  var y=8;
+  var div=x/y;
+  document.write(div)
+}
